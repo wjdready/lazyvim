bf2961dbf3a9a89dc12b6006da644ca580f24870
weijundong <wjdready@163.com>
2024-06-17 12:19:36 +0800

    添加 one_monikai 主题

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,10 @@
-
 return {
     { "lukas-reineke/indent-blankline.nvim", enabled = false },
+    { "cpea2506/one_monokai.nvim" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "one_monokai",
+        },
+    },
 }
